62a0a4838540a0cabfc6542e7bd245e39c721437
James Pier <jpier2012@gmail.com>
2019-04-15 13:32:32 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -6,7 +6,7 @@ def my_find(collection)
   }
 end
 
-def collection_testing(collection)
+def collection_testing
     i = 0
     collect_test = [1,2,3,4,5]
     for i in collect_test
@@ -14,3 +14,5 @@ def collection_testing(collection)
         i += 1
       end
 end
+
+puts collection
