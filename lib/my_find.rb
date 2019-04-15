7e8a8f8c9560828d590d4cdd558cf2a4f42ad8ee
James Pier <jpier2012@gmail.com>
2019-04-15 13:32:10 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,7 +1,19 @@
 require 'pry'
 
 def my_find(collection)
+
+  i = 0
+  collect_test = [1,2,3,4,5]
+  for i in collect_test
+    if i == 3 return i
+      i += 1
+    end
+  
   collection.each {|element|
     if yield(element) return yield(element)
   }
+end
+
+def collection_testing(collection)
+
 end
