1be8e399d0ad3cef357861e490381adf5cd3cd5f
James Pier <jpier2012@gmail.com>
2019-04-15 13:36:49 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection)
   new_array = []
   collection.each {|element|
-    new_array << element if yield(element) 
+    if yield(element) return elemet
   }
   new_array
 end
